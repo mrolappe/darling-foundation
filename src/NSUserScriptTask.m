a3e410be7c63c56afe259b3577e17038bdf1ec5b
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2022-07-15 14:19:38 +0200

    NSUserScriptTask stub

---
+++ src/NSUserScriptTask.m
@@ -0,0 +1,28 @@
+#import <Foundation/Foundation.h>
+
+@interface NSUserScriptTask : NSObject
+@end
+
+@implementation NSUserScriptTask
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    NSLog(@"Stub called: %@ in %@", NSStringFromSelector([anInvocation selector]), [self class]);
+}
+@end
+
+
+@interface NSUserAppleScriptTask : NSUserScriptTask
+@end
+
+@implementation NSUserAppleScriptTask
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    return [NSMethodSignature signatureWithObjCTypes: "v@:"];
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    NSLog(@"Stub called: %@ in %@", NSStringFromSelector([anInvocation selector]), [self class]);
+}
+@end
